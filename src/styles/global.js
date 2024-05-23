ef50343ec4ba58bf184c7acc14e37e1490d11ce7
Gabriel Marini <gabrielmarini12@icloud.com>
2024-05-22 21:09:56 -0300

    criado os cards de resumo (entrada, saída e total)  e o contaúdo interno de cada card.

--- src/styles/global.js
+++ src/styles/global.js
@@ -3,9 +3,10 @@ import { createGlobalStyle } from "styled-components";
 const Global = createGlobalStyle`
 
     * {
-margin: 0;
-padding: 0;
+        margin: 0;
+        padding: 0;
     }
+    
     body {
         font-family: 'Poppins', Sans-Serif;
         background-color: #f2f2f2;
